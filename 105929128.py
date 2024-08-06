8d7d0a1fa06f3ccc0d8b7c42621325aa25e40d12
Zimo-07 <zjiu0255@gmail.com>
2024-08-06 12:48:49 +0800

    Add files via upload

--- 105929128.py
+++ 105929128.py
@@ -1,39 +1,44 @@
 import random
 
+lists = []
+
 digit1 = random.randint(1, 100)
 digit2 = random.randint(1, 100)
-a = True
 
 while digit1 % digit2 != 0:
     digit2 = random.randint(1, 100)
+
+while True:
+    usd = input("請輸入運算符號(1)+ (2)- (3)* (4)/")
+    if usd == "1":
+        a = digit1 + digit2 
+        break
     
+    elif usd == "2":
+        a = digit1 - digit2
+        break
+    
+    elif usd == "3":
+        a = digit1 * digit2
+        break
+    
+    elif usd == "4":
+        a = digit1 / digit2
+        for i in range(1, 101):
+            
+            if digit1 % i == 0:
+                lists.append(i)                     
+        break
+    
+    else:
+        print("輸入代號")
+        break
+        
 print("第一個數值是:", digit1)
 print("第二個數值是:", digit2)
+print("計算結果為:", a)
+print("可被整除的數有:", lists)
 
-while a:
-    user = input("請輸入想運算符號(1)+ (2)- (3)* (4)/")
-    if user == "1":
-        sum = digit1 + digit2 
-        print("計算結果為:", sum)
-        a = False
-        
-    elif user == "2":
-        sum = digit1 - digit2
-        print("計算結果為:", sum)
-        a = False
-        
-    elif user == "3":
-        sum = digit1 * digit2
-        print("計算結果為:", sum)
-        a = False
-        
-    elif user == "4":
-        sum = digit1 / digit2
-        print("計算結果為:", sum) 
-        a = False
-            
-    else:
-        print("請從1~4之間選擇")
 
 
 
