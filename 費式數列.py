7723c841f61fa6dcad194d5cbb9488ca208091b9
Zimo-07 <zjiu0255@gmail.com>
2024-08-09 17:35:44 +0800

    Update and rename sample2.py to 費式數列.py

---
+++ 費式數列.py
@@ -0,0 +1,19 @@
+
+a = 0
+b = 1
+c = 1
+
+lists = []
+
+user = int(input('計算階層')
+           
+for i in user:
+    lists.append(a)
+    lists.append(b)
+    lists.append(c)
+
+    c = a + b
+    b = a + c
+    a = b + c
+    
+print(lists)
